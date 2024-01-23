a5e526e0c572ad96b658d64d0f06112cff772325
Dylan Conley <dylanconley@shaw.ca>
2024-01-23 15:57:53 -0700

    Added some legends

--- mainPart2.m
+++ mainPart2.m
@@ -9,7 +9,8 @@ x = -5000:100:5000;
 %2.1 Part A
 Gz = gz(p1,p2,R,z,x);
 
-plot(x,Gz,".-")
+h1 = plot(x,Gz,".-")
+legend([h1], '4260kg/m^{3} 275m Deep Sphere', 'Location', 'Best');
 hold off;
 
 
@@ -24,8 +25,14 @@ Z = sqrt(xmesh.^2+ymesh.^2);
 
 Gz2 = gz(p1,p2,R,z,Z);
 
-mesh(xmesh,ymesh,Gz2)
+h1 = mesh(xmesh,ymesh,Gz2)
 colorbar;
+title("2D Gravitational Attraction of a Sphere 275m Deep")
+xlabel('X Distance (meters) in 100m Increments')
+ylabel('Y Distance (meters) in 100m Increments')
+zlabel('Gravitational Attraction (mGal)')
+legend([h1], '4260kg/m^{3} 275m Deep Sphere', 'Location', 'Best');
+
 grid on
 
 %2.2 Part A
@@ -34,27 +41,33 @@ z1 = 350;
 
 Gz3 = gz(p1,p2,R,z1,x);
 
-plot(x,Gz,'.-')
+h1 = plot(x,Gz,'.-')
 hold on
-plot(x,Gz3,'.-')
+h2 = plot(x,Gz3,'.-')
+legend([h1,h2],'4260kg/m^{3} 275m Deep Sphere', '4260kg/m^{3} 350m Deep Sphere', 'Location', 'Best');
+
 figure
 %2.2 Part B
 p3 = 5760;
 
-Gz4 = gz(p1,p3,R,z,x);
+Gz4 = gz(p1,p3,R,z1,x);
 
-plot(x,Gz,'.-')
+h1 = plot(x,Gz,'.-')
 hold on
-plot(x,Gz4,'.-')
+h2 = plot(x,Gz4,'.-')
+legend([h1,h2],'4260kg/m^{3} 275m Deep Sphere', '5760/m^{3} 350m Deep Sphere', 'Location', 'Best');
+
+
 
 %2.2 Part C
 Gzx = gzx(p1,p2,R,z,x);
 Gzx1 = gzx(p1,p3,R,z1,x);
 
 figure
-plot(x,Gzx,'.-')
+h1 = plot(x,Gzx,'.-')
 hold on
-plot(x,Gzx1,'.-')
+h2 = plot(x,Gzx1,'.-')
+legend([h1,h2],'4260kg/m^{3} 275m Deep Sphere', '5760kg/m^{3} 350m Deep Sphere', 'Location', 'Best');
 
 %2.3 Part A
 a = 100;
@@ -68,6 +81,11 @@ plot(x,Gz,'.-')
 %2.3 Part B
 gz2_cyl = cylinderGz(a,z,p1,p2,Z);
 figure
-mesh(xmesh,ymesh,gz2_cyl,FaceColor="blue",FaceAlpha="0.25")
+h1 = mesh(xmesh,ymesh,gz2_cyl,FaceColor="blue",FaceAlpha="0.25")
 hold on
-mesh(xmesh,ymesh,Gz2,FaceColor = "red")
+h2 = mesh(xmesh,ymesh,Gz2,FaceColor = "red")
+title("2D Gravitational Attraction of a Cylinder vs a Sphere 275m Deep")
+xlabel('X Distance (meters) in 100m Increments')
+ylabel('Y Distance (meters) in 100m Increments')
+zlabel('Gravitational Attraction (mGal)')
+legend([h1,h2], '4260kg/m^{3} 275m Deep Cylinder','4260kg/m^{3} 275m Deep Sphere', 'Location', 'Best');
