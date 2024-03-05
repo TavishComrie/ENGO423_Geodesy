e53535736a5c089e9c3b968dbd8d1eac819a88a8
TavishComrie <113808140+TavishComrie@users.noreply.github.com>
2024-03-05 16:19:30 -0700

    Update ENGO423_Lab3_main.m

--- Lab3/ENGO423_Lab3_main.m
+++ Lab3/ENGO423_Lab3_main.m
@@ -1,37 +1,50 @@
 close all;
 
-theta = -180:0.01:180;
+theta = 0:180;
 t = cosd(theta);
 
 for i = 1:size(t,2)
-    P = Pnm(21,t(1,i));
+    P = Pnm(20,t(1,i));
     Results(i,1) = P(16,16); 
     Results(i,2) = P(16,6);
     Results(i,3) = P(16,1);
-    check(i,1) = P(2,2);
    
 end
 
 
-P = Pnm(20,cosd(0));
 
 
 figure
-Results1 = plot(theta,Results(i,1),'.-');
+Results1 = plot(theta,Results(:,1),'-');
 
-title('Gravitational Attraction of a Sphere against a Cylinder');
-xlabel('X Distance (meters) in 100m increments');
-ylabel('Gravitational Attraction (mGal)');
-hold off
-%{
-figure
-Results2 = plot(theta,Results(i,3),'.-');
+title('Normalized Legendre function for m = 15');
+xlabel('Co-latitude [degrees]');
+xlim([0 180]);
+ylabel('P of index 15,15');
+ylim([-5 5]);
+grid on
 hold off
 
 figure
-Results3 = plot(theta,Results(i,3),'.-');
+Results2 = plot(theta,Results(:,2),'-');
+title('Normalized Legendre function for m = 5');
+xlabel('Co-latitude [degrees]');
+xlim([0 180]);
+ylabel('P of index 15,5');
+ylim([-5 5]);
+grid on
 hold off
-%}
+
+figure
+Results3 = plot(theta,Results(:,3),'-');
+title('Normalized Legendre function for m = 0');
+xlabel('Co-latitude [degrees]');
+xlim([0 180]);
+ylabel('P of index 15,0');
+ylim([-5 7]);
+grid on
+hold off
+
 
 
 
