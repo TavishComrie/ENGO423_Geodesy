aef7afc012b39799610862ed9a904923c709098f
Dylan Conley <dylanconley@shaw.ca>
2024-03-07 20:08:08 -0700

    Did some of part 3

--- Lab3/ENGO423_Lab3_main.m
+++ Lab3/ENGO423_Lab3_main.m
@@ -59,3 +59,38 @@ for i = 1:size(nVals,2)
         sum(1,i) = sum(1,i) + P(n+1,mindex)^2;
     end
 end
+
+
+SphericalHarmonics(20,15,15);
+SphericalHarmonics(20,15,5);
+SphericalHarmonics(20,15,0);
+
+
+function [Rnm,Snm] = SphericalHarmonics(nmax,n,m)
+    PnmVal(181,1) = zeros;
+    Rnm(181,361) = zeros;
+    Snm(181,361) = zeros;
+
+    for theta = 0:180
+        P = Pnm(nmax+1,cosd(theta));
+        PnmVal(theta+1,1)=P(n+1,m+1);
+        for lambda = 0:360
+            Rnm(theta+1,lambda+1) = PnmVal(theta+1,1)*cos(m*lambda);
+            Snm(theta+1,lambda+1) = PnmVal(theta+1,1)*sin(m*lambda);
+        end
+    end
+
+    G = Rnm + Snm; %I don't really know about this
+
+    figure;
+    hold on;
+    imagesc([0 360],[0 180],G)
+    xlabel(['Longitude, ' char(176)])
+    ylabel(['Lattitude, ' char(176)])
+    xlim([0 360])
+    ylim([0 180])
+    titlePhrase = ['Spherical Harmonic for n=' num2str(n) ',m=' num2str(m) ')'];
+    title(titlePhrase)
+    colorbar
+end
+
